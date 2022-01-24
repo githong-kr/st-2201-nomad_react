0389c109fa52aa82f74272281857024c9fdf3369
bale <githong@kakao.com>
2022-01-24 23:03:46 +0900

    toDo by using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import { useState, useEffect } from 'react';
-
-function Hello() {
-  useEffect(() => {
-    console.log(`Hello Component Created!`);
-    return () => {
-      console.log(`Hello Component Destroyed!`);
-    };
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from 'react';
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') return;
+    setToDos((prevToDos) => [toDo, ...toDos]);
+    setToDo('');
+  };
   return (
     <div>
-      {showing === true ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? 'HIDE' : 'SHOW'}</button>
+      <h1>My To Dos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
