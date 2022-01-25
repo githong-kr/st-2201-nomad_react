769de7b57f7ad8462dfcc325cc01b586093b62fd
bale <githong@kakao.com>
2022-01-25 21:31:35 +0900

    make coin tracker

--- src/App.js
+++ src/App.js
@@ -1,33 +1,64 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === '') return;
-    setToDos((prevToDos) => [toDo, ...toDos]);
-    setToDo('');
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [index, setIndex] = useState(-1);
+
+  const onSelect = (event) => {
+    setIndex(event.target.value);
   };
+  const onChange = (event) => setMoney(event.target.value);
+
+  useEffect(() => {
+    const getCoins = async () => {
+      const result = await fetch(`https://api.coinpaprika.com/v1/tickers`);
+      setCoins(await result.json());
+      setLoading(false);
+    };
+    getCoins();
+  }, []);
+
   return (
     <div>
-      <h1>My To Dos {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? null : ` (${coins.length})`}</h1>
+      {loading ? (
+        `Loading...`
+      ) : (
+        <div>
+          <div>
+            <input
+              onChange={onChange}
+              value={money}
+              type="number"
+              placeholder="Type your money..."
+            />
+          </div>
+          <hr />
+          <div>
+            <select onChange={onSelect}>
+              <option value="-1">Select the coin...</option>
+              {coins.map((item, index) => {
+                return (
+                  <option
+                    key={item.id}
+                    value={index}
+                  >{`${item.name} (${item.symbol}) : $${item.quotes.USD.price} USD`}</option>
+                );
+              })}
+            </select>
+          </div>
+          <div>
+            {index === -1 ? null : (
+              <h2>
+                You can buy {money / coins[index].quotes.USD.price}
+                {coins[index].symbol}
+              </h2>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
